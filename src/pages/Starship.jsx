86fc4247267fece0106e0003e253f907797b8651
Mariluz Vargas <mariluzv88@gmail.com>
2023-07-28 10:10:31 -0400

    added api to vehicle and starships pages

--- src/pages/Starship.jsx
+++ src/pages/Starship.jsx
@@ -10,14 +10,23 @@ function Starship() {
   const loaded = () => {
     return (
       <div>
+        <div className='shipContainer'>
             {url? url.map((card)=>{
             return (
-             
-                  <h1>{card.name} {card.model}</h1>
-              
+                <div className='ship'>
+                  <h3 ><h3 className='title'>Name:</h3>{card.name} <h3 className='title'>Model:</h3>  {card.model}</h3>
+                  <h4 > <h3 className='title'>Manufacturer:</h3>{card.manufacturer}</h4>
+                  <div className='statsContainer'>
+                        <div className='stats'><h4><h3 className='title'>Cost in Credits:</h3>{card.cost_in_credits}<h4 className='title'>Length:</h4> {card.length} </h4></div>
+                        <div className='stats'> <h4><h4 className='title'>Max Speed: </h4>{card.max_atmosphering_speed} </h4><h4 className='title'>Rations: </h4><h4>{card.max_atmosphering_speed} </h4> </div>
+                        <div className='stats'><h4><h4 className='title'>Passengers: </h4> {card.passengers} <h4 className='title'>Cargo Capacity:</h4>{card.cargo_capacity}</h4></div>
+                        <div className='stats'><h3><h3 className='title'>Name:</h3>HyperDrive Rating:{card.hyperdrive_rating}<h3 className='title'>Name:</h3>Crew:{card.crew}</h3></div>
+                    </div>
+                </div>
             )
-           }):<p>Loading</p>}
+          }):<p>Loading</p>}
            {/* <h1>{url.name}</h1>  */}
+          </div>
            
       </div>
     )
